03841f943fd6159a2b9bf03a80cefc79ea18925b
Lucas Message <lucas.message@yahoo.com>
2017-01-28 19:21:21 -0600

    Works when device is restarted.

---
+++ app/src/main/java/com/bitbite/autosilencer/autoStart.java
@@ -0,0 +1,17 @@
+package com.bitbite.autosilencer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Lucas on 1/28/2017.
+ */
+
+public class autoStart extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, StayAliveService.class));
+    }
+}
